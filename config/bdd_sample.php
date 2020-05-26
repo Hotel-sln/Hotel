005896b1fbe1e98e7f0df417e557729b6adf6332
Sam Montaudon <sam.montaudon@gmail.com>
2020-05-26 15:49:57 +0200

    Creation de bdd_sample

---
+++ config/bdd_sample.php
@@ -0,0 +1,9 @@
+<?php
+    define('SERVER', 'localhost');
+    define('NAME', 'hotel_menton');
+    define('USER', 'root');
+    define('PASSWD', '');
+    $server = 'localhost';
+    $name ='hotel_menton'; 
+    $user='root';
+    $passwd='';
