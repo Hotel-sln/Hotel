5aae8a77c7990ab88900f262f7315318862c0e0b
Sam Montaudon <sam.montaudon@gmail.com>
2020-05-26 10:46:11 +0200

    rearrangement des views et creation de deux controllers

---
+++ views/footer.php
@@ -0,0 +1,38 @@
+<footer class="footermain">
+    <ul class="feetlist">
+        Une question ? Nous contacter:
+        <li class="feetelem">9 impasse du Scie Tronc</li>
+        <li class="feetelem">(+33)04 69 42 06 91</li>
+        <li class="feetelem">Hotel-Menton@hotmail.fr</li>
+        <li class="feetelem">
+            <span class="network">
+                <button class="btnfeet" onclick="window.location.href = 'https://twitter.com/';">
+                    <i class="fab fa-twitter-square fa-2x"> </i>
+                </button>
+                <button class="btnfeet" onclick="window.location.href = 'https://www.facebook.com/';">
+                    <i class="fab fa-facebook-square fa-2x"></i>
+                </button>
+                <button class="btnfeet" onclick="window.location.href = 'https://www.instagram.com/';">
+                    <i class="fab fa-instagram fa-2x"></i>
+                </button>
+            </span>
+        </li>
+    </ul>
+
+    <aside class="legal">
+        Mentions légales - Plan du site - © L'escadev
+    </aside>
+</footer>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
+<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+<script src="https://kit.fontawesome.com/86d37fbec9.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="assets/js/diapo.js"></script>
+
+<script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
