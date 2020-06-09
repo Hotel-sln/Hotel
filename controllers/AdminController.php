e82e8c7099e5a56d45800a4d05b1ec52c3d8e07c
Nicolas Vauché <hello@nicolasvauche.net>
2020-06-09 17:26:10 +0200

    Fin.

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,27 +1,69 @@
 <?php
-require('models/Chambre.php');
+require( 'models/Chambre.php' );
 
-function listechambresAction(){
-$chambreObject = new Chambre();
-$chambres = $chambreObject->getChambres();
-$pageTitle = 'Liste des chambres';
-    require('views/admin/listechambres.php');
+function listechambresAction()
+{
+    $chambreObject = new Chambre();
+    $chambres      = $chambreObject->getChambres();
+    $pageTitle     = 'Liste des chambres';
+    require( 'views/admin/listechambres.php' );
 }
 
-function editchambreAction(){
-    $requestUri = str_replace(BASE_URL, '', $_SERVER['REQUEST_URI']);
-    $requestParams = explode('/', $requestUri);
-    $chambreId =  isset($requestParams[2]) ? $requestParams[2] : null;
+function editchambreAction()
+{
+    $requestUri    = str_replace( BASE_URL, '', $_SERVER['REQUEST_URI'] );
+    $requestParams = explode( '/', $requestUri );
+    $chambreId     = isset( $requestParams[2] ) ? $requestParams[2] : null;
     $chambreObject = new Chambre();
-    $chambre = $chambreObject->getChambre($chambreId);
+    $chambre       = $chambreObject->getChambre( $chambreId );
 
-    if (isset($_POST['forminscription2'])) {
-        $chambreObject->changeChambre($chambreId);
-        header('Location: ' . BASE_URL . 'admin/editchambre/'. $chambre['id'] .'');
+    if( isset( $_POST['forminscription2'] ) ) {
+        $chambreObject->changeChambre( $chambreId );
+        header( 'Location: ' . BASE_URL . 'admin/editchambre/' . $chambre['id'] . '' );
     }
 
-
     $pageTitle = 'Modifier une chambre';
-    require('views/admin/editchambre.php');
-  
+    require( 'views/admin/editchambre.php' );
+}
+
+function ajoutchambreAction()
+{
+    if( isset( $_POST['submit'] ) ) {
+        // 1. Récupération des données du formulaire
+        $chambreNom         = htmlspecialchars( $_POST['txt_chambre'] );
+        $chambreType        = htmlspecialchars( $_POST['txt_type'] );
+        $chambreDescription = htmlspecialchars( $_POST['txt_description'] );
+        $chambrePhoto       = htmlspecialchars( $_POST['txt_imgs'] );
+        $chambrePrix        = htmlspecialchars( $_POST['txt_prix'] );
+
+        $params = array(
+            'chambre'      => $chambreNom,
+            'type_chambre' => $chambreType,
+            'description'  => $chambreDescription,
+            'imgs'         => $chambrePhoto,
+            'prix'         => $chambrePrix,
+        );
+
+        // 2. Appel du modèle
+        $chambreObject = new Chambre();
+        $chambreObject->ajouterChambre( $params );
+
+        // 3. Redirection vers la liste des chambres
+        Header( 'Location: ' . BASE_URL . 'admin/listechambres' );
+    }
+
+    $pageTitle = 'Ajouter une chambre';
+    require( 'views/admin/ajoutchambre.php' );
+}
+
+function supprimechambreAction()
+{
+    $requestUri    = str_replace( BASE_URL, '', $_SERVER['REQUEST_URI'] );
+    $requestParams = explode( '/', $requestUri );
+    $chambreId     = isset( $requestParams[2] ) ? $requestParams[2] : null;
+
+    $chambreObject = new Chambre();
+    $chambreObject->supprimerChambre( $chambreId );
+
+    Header( 'Location: ' . BASE_URL . 'admin/listechambres' );
 }
--- models/Chambre.php
+++ models/Chambre.php
@@ -1,50 +1,70 @@
 <?php
-require('models/Bdd.php');
+require( 'models/Bdd.php' );
 
 class Chambre
 {
+
     public function getChambres()
     {
-        $bdd = new Bdd();
+        $bdd        = new Bdd();
         $connection = $bdd->getConnection();
-        $result = $connection->query('SELECT * FROM chambres ORDER BY id DESC');
-        return $result ? $result->fetchAll(PDO::FETCH_ASSOC) : null;
+        $result     = $connection->query( 'SELECT * FROM chambres ORDER BY id DESC' );
+
+        return $result ? $result->fetchAll( PDO::FETCH_ASSOC ) : null;
     }
-    
-    public function getChambre($chambreId)
+
+    public function getChambre( $chambreId )
     {
-        $bdd = new Bdd();
+        $bdd        = new Bdd();
         $connection = $bdd->getConnection();
-        $result = $connection->query('SELECT * FROM chambres where id = ' . $chambreId);
-        return $result ? $result->fetch(PDO::FETCH_ASSOC) : null;
+        $result     = $connection->query( 'SELECT * FROM chambres where id = ' . $chambreId );
+
+        return $result ? $result->fetch( PDO::FETCH_ASSOC ) : null;
     }
-    
-    public function changeChambre($chambreId)
+
+    public function changeChambre( $chambreId )
     {
-        $Chambre = htmlspecialchars($_POST['txt_chambre']);
-        
-        $ChambreTypes = htmlspecialchars($_POST['txt_type']);
-        $Description = htmlspecialchars($_POST['txt_description']);
-        $Imgs = htmlspecialchars($_POST['txt_imgs']);
-        $Prix = htmlspecialchars($_POST['txt_prix']);
-        
+        $Chambre = htmlspecialchars( $_POST['txt_chambre'] );
 
+        $ChambreTypes = htmlspecialchars( $_POST['txt_type'] );
+        $Description  = htmlspecialchars( $_POST['txt_description'] );
+        $Imgs         = htmlspecialchars( $_POST['txt_imgs'] );
+        $Prix         = htmlspecialchars( $_POST['txt_prix'] );
 
-        $bdd = new Bdd();
+        $bdd        = new Bdd();
         $connection = $bdd->getConnection();
-        $result = $connection->prepare("UPDATE chambres SET  chambre = :chambre , type_chambre = :type_chambre , description = :description , imgs = :imgs , prix = :prix  WHERE id= $chambreId ");
-        $result->execute(array(
+        $result     =
+            $connection->prepare( "UPDATE chambres SET  chambre = :chambre , type_chambre = :type_chambre , description = :description , imgs = :imgs , prix = :prix  WHERE id= $chambreId " );
+        $result->execute( array(
 
-            'chambre' => $Chambre,
+            'chambre'      => $Chambre,
             'type_chambre' => $ChambreTypes,
-            
-            'description' => $Description,
-            'imgs' => $Imgs,
-            'prix' => $Prix,
 
-        ));
-       
-        
+            'description' => $Description,
+            'imgs'        => $Imgs,
+            'prix'        => $Prix,
+
+        ) );
     }
 
+    public function ajouterChambre( $params )
+    {
+        $bdd        = new Bdd();
+        $connection = $bdd->getConnection();
+
+        $result =
+            $connection->prepare( 'INSERT INTO chambres(chambre, type_chambre, description, imgs, prix) VALUES(:chambre, :type_chambre, :description, :imgs, :prix)' );
+        $result->execute( $params );
+    }
+
+    public function supprimerChambre( $id )
+    {
+        $bdd        = new Bdd();
+        $connection = $bdd->getConnection();
+
+        $result = $connection->prepare( 'DELETE FROM chambres WHERE id = :id' );
+        $result->execute( array(
+            'id' => $id,
+        ) );
+    }
 }
