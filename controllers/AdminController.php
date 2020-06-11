76ce596f05875895cc0500766d6192b083c41a05
Nicolas Vauché <hello@nicolasvauche.net>
2020-06-11 15:12:10 +0200

    clean

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,46 +1,74 @@
 <?php
-require('models/Chambre.php');
-require('models/Admin.php');
+require( 'models/Chambre.php' );
+require( 'models/Admin.php' );
+
+function isConn()
+{
+    if( ! isset( $_SESSION["adminid"] ) && ( ! isset( $_SESSION["adminmail"] ) ) ) {
+        header( 'Location: ' . BASE_URL . 'admin/login' );
+    }
+}
+
+function indexAction()
+{
+    isConn();
+    require( "views/admin/index.php" );
+}
+
+function loginAction()
+{
+    if( isset( $_POST['formconnect'] ) ) {
+        $mailconnect = htmlspecialchars( $_POST['mailconnect'] );
+        $admin       = new Admin();
+        $result      = $admin->connexionAdmin( $mailconnect );
+
+        if( $_POST['mdpconnect'] == $result['motdepasse'] ) {
+            session_start();
+            $_SESSION['id']   = $result['id'];
+            $_SESSION['mail'] = $result['mail'];
+            header( 'Location: ' . BASE_URL . 'admin/' );
+        }
+    }
+    require( "views/admin/login.php" );
+}
+
 function listechambresAction()
 {
-    session_start();
     isConn();
     $chambreObject = new Chambre();
     $chambres      = $chambreObject->getChambres();
     $pageTitle     = 'Liste des chambres';
-    require('views/admin/listechambres.php');
+    require( 'views/admin/listechambres.php' );
 }
 
 function editchambreAction()
 {
-    session_start();
     isConn();
-    $requestUri    = str_replace(BASE_URL, '', $_SERVER['REQUEST_URI']);
-    $requestParams = explode('/', $requestUri);
-    $chambreId     = isset($requestParams[2]) ? $requestParams[2] : null;
+    $requestUri    = str_replace( BASE_URL, '', $_SERVER['REQUEST_URI'] );
+    $requestParams = explode( '/', $requestUri );
+    $chambreId     = isset( $requestParams[2] ) ? $requestParams[2] : null;
     $chambreObject = new Chambre();
-    $chambre       = $chambreObject->getChambre($chambreId);
+    $chambre       = $chambreObject->getChambre( $chambreId );
 
-    if (isset($_POST['forminscription2'])) {
-        $chambreObject->changeChambre($chambreId);
-        header('Location: ' . BASE_URL . 'admin/editchambre/' . $chambre['id'] . '');
+    if( isset( $_POST['forminscription2'] ) ) {
+        $chambreObject->changeChambre( $chambreId );
+        header( 'Location: ' . BASE_URL . 'admin/editchambre/' . $chambre['id'] . '' );
     }
 
     $pageTitle = 'Modifier une chambre';
-    require('views/admin/editchambre.php');
+    require( 'views/admin/editchambre.php' );
 }
 
 function ajoutchambreAction()
 {
-    session_start();
     isConn();
-    if (isset($_POST['submit'])) {
+    if( isset( $_POST['submit'] ) ) {
         // 1. Récupération des données du formulaire
-        $chambreNom         = htmlspecialchars($_POST['txt_chambre']);
-        $chambreType        = htmlspecialchars($_POST['txt_type']);
-        $chambreDescription = htmlspecialchars($_POST['txt_description']);
-        $chambrePhoto       = htmlspecialchars($_POST['txt_imgs']);
-        $chambrePrix        = htmlspecialchars($_POST['txt_prix']);
+        $chambreNom         = htmlspecialchars( $_POST['txt_chambre'] );
+        $chambreType        = htmlspecialchars( $_POST['txt_type'] );
+        $chambreDescription = htmlspecialchars( $_POST['txt_description'] );
+        $chambrePhoto       = htmlspecialchars( $_POST['txt_imgs'] );
+        $chambrePrix        = htmlspecialchars( $_POST['txt_prix'] );
 
         $params = array(
             'chambre'      => $chambreNom,
@@ -52,66 +80,25 @@ function ajoutchambreAction()
 
         // 2. Appel du modèle
         $chambreObject = new Chambre();
-        $chambreObject->ajouterChambre($params);
+        $chambreObject->ajouterChambre( $params );
 
         // 3. Redirection vers la liste des chambres
-        Header('Location: ' . BASE_URL . 'admin/listechambres');
+        Header( 'Location: ' . BASE_URL . 'admin/listechambres' );
     }
 
     $pageTitle = 'Ajouter une chambre';
-    require('views/admin/ajoutchambre.php');
+    require( 'views/admin/ajoutchambre.php' );
 }
 
 function supprimechambreAction()
 {
-    session_start();
-   
     isConn();
-    $requestUri    = str_replace(BASE_URL, '', $_SERVER['REQUEST_URI']);
-    $requestParams = explode('/', $requestUri);
-    $chambreId     = isset($requestParams[2]) ? $requestParams[2] : null;
+    $requestUri    = str_replace( BASE_URL, '', $_SERVER['REQUEST_URI'] );
+    $requestParams = explode( '/', $requestUri );
+    $chambreId     = isset( $requestParams[2] ) ? $requestParams[2] : null;
 
     $chambreObject = new Chambre();
-    $chambreObject->supprimerChambre($chambreId);
+    $chambreObject->supprimerChambre( $chambreId );
 
-    Header('Location: ' . BASE_URL . 'admin/listechambres');
-}
-
-function isConn(){
-    if(!isset($_SESSION["adminid"]) && (!isset($_SESSION["adminmail"]))){
-
-        header('Location: ' . BASE_URL . 'admin/login');
-    }
-}
-
-function loginAction()
-{
-    session_start();
-    
-    if (isset($_POST['formconnect'])) {
-        $mailconnect =  htmlspecialchars($_POST['mailconnect']);
-        $admin= new Admin();
-        $result = $admin->connexionAdmin($mailconnect);
-        
-       
-        if ($_POST['mdpconnect'] == $result['motdepasse']) { 
-            session_start();
-            $_SESSION['id']= $result['id'];
-            $_SESSION['mail'] = $result['mail'];
-           
-           
-            
-            header('Location: ' . BASE_URL . 'admin/');
-           
-            
-            
-        }
-    }
-    require("views/admin/login.php");
-}
-
-function indexAction(){
-    
-    isConn();
-    require("views/admin/index.php");
+    Header( 'Location: ' . BASE_URL . 'admin/listechambres' );
 }
